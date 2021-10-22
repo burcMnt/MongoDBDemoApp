e5faecf2854e290a4e0530b4a3412a96e9abd8a9
burcMnt <burcumantar0@gmail.com>
2021-10-23 02:01:38 +0300

    changed model

--- MongoDBDemoApp/Program.cs
+++ MongoDBDemoApp/Program.cs
@@ -8,7 +8,21 @@ namespace MongoDBDemoApp
         static void Main(string[] args)
         {
             MongoCRUD db = new MongoCRUD("AddressBook");
-            db.InsertRecord("Users", new PersonModel() { FirstName = "Ece", LastName = "Yayal" });
+           // db.InsertRecord("Users", new PersonModel() { FirstName = "Ece", LastName = "Yayal" });
+            PersonModel person = new()
+            {
+                FirstName = "Cem",
+                LastName = "Cünedi",
+                PrimaryAddress = new AddressModel()
+                {
+                    StreetAddress = "101 Oak Street",
+                    City = "Scranton",
+                    State = "PA",
+                    ZipCode = "18512"
+                }
+            };
+            db.InsertRecord("Users", person);
+
             Console.ReadLine();
         }
     }
