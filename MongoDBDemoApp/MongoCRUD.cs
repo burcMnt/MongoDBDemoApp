4f2d5e0f8c12ea347299c059284d0307c7852568
burcMnt <burcumantar0@gmail.com>
2021-10-23 17:53:39 +0300

    Read Operation

--- MongoDBDemoApp/MongoCRUD.cs
+++ MongoDBDemoApp/MongoCRUD.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Driver;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,13 @@ namespace MongoDBDemoApp
         {
             var collection = db.GetCollection<T>(table);
             collection.InsertOne(record);
+        }
+
+        public List<T> LoadRecords<T>(string table)
+        {
+            var collection = db.GetCollection<T>(table);
+            return collection.Find(new BsonDocument()).ToList();
+
         }
     }
 }
--- MongoDBDemoApp/Program.cs
+++ MongoDBDemoApp/Program.cs
@@ -8,20 +8,37 @@ namespace MongoDBDemoApp
         static void Main(string[] args)
         {
             MongoCRUD db = new MongoCRUD("AddressBook");
-           // db.InsertRecord("Users", new PersonModel() { FirstName = "Ece", LastName = "Yayal" });
-            PersonModel person = new()
+            //POST OPERATION
+
+            // db.InsertRecord("Users", new PersonModel() { FirstName = "Ece", LastName = "Yayal" });
+            //PersonModel person = new()
+            //{
+            //    FirstName = "Cem",
+            //    LastName = "Cünedi",
+            //    PrimaryAddress = new AddressModel()
+            //    {
+            //        StreetAddress = "101 Oak Street",
+            //        City = "Scranton",
+            //        State = "PA",
+            //        ZipCode = "18512"
+            //    }
+            //};
+            //db.InsertRecord("Users", person);
+
+        /**************************************************************/
+        //GET OPERATION
+
+            var recs = db.LoadRecords<PersonModel>("Users");
+            foreach (var item in recs)
             {
-                FirstName = "Cem",
-                LastName = "Cünedi",
-                PrimaryAddress = new AddressModel()
+                Console.Write($"PersonId = {item.Id}, Person FullName= {item.FirstName} {item.LastName} ");
+
+                if (item.PrimaryAddress !=null)
                 {
-                    StreetAddress = "101 Oak Street",
-                    City = "Scranton",
-                    State = "PA",
-                    ZipCode = "18512"
+                    Console.Write($", Person's City = {item.PrimaryAddress.City}");
                 }
-            };
-            db.InsertRecord("Users", person);
+                Console.WriteLine();
+            }
 
             Console.ReadLine();
         }
